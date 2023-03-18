b872906c91ef8fda1e66a8e3ccda63bf7a123140
Alexander Tereshkov <atserashkou@gmail.com>
2023-03-18 11:47:58 +0100

    Revert, add only TG logger

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,4 +1,6 @@
 import { createLogger, format, transports } from 'winston';
+
+import TelegramLogger from 'winston-telegram';
  
 const logLevels = {
     fatal: 0,
@@ -12,7 +14,16 @@ const logLevels = {
 const logger = createLogger({
     levels: logLevels,
     format: format.combine(format.timestamp(), format.json()),
-    transports: [new transports.Console({ level: 'trace' })],
+    transports: [
+        new transports.Console({ level: 'trace' }), // Regular console
+        new TelegramLogger({
+            token: process.env.TG_LOG_BOT_TOKEN,
+            chatId: process.env.TG_LOG_CHAT_ID,
+            level: 'info',
+            unique: true
+        })
+    ],
+
 });
 
 export default logger;
